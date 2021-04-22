496cff3e95f1c4462437a993d562976fdabe68a5
Dibble710 <79325792+Dibble710@users.noreply.github.com>
2021-04-22 16:00:21 -0400

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -29,47 +29,49 @@ export default function Project() {
           Here are a few of my completed projects!
         </h2>
         <section className="grid grid-cols-2 gap-8">
-          {projectData && projectData.map((project,index) => (
-          <article className="relative rounded-lg shadow-xl bg-white p-16">
-            <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
-              <a
-                href={project.link}
-                alt={project.title}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {project.title}
-              </a>
-            </h3>
-            <div className="text-gray-500 text-xs space-x-4">
-              <span>
-                <strong className="font-bold">Finished on</strong>:{" "}
-                {new Date(project.date).toLocaleDateString()}
-              </span>
-              <span>
-                <strong className="font-bold">Company</strong>: {project.place}
-              </span>
-              <span>
-                <strong className="font0bold">Type</strong>:{" "}
-                {project.projectType}
-              </span>
-              <p className="my-6 text-lg text-gray-700 leading-relaxed">
-                {project.description}
-              </p>
-              <a
-                href={project.link}
-                rel="noopener noreferrer"
-                targer="_blank"
-                className="text-green-500 font-bold hover:underline hover:text-red-400 text-xl"
-              >
-                View The Project{" "}
-                <span role="img" aria-label="right pointer">
-                👉
-                </span>
-              </a>
-            </div>
-          </article>
-          ))}
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font0bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-green-500 font-bold hover:underline hover:text-red-400 text-xl"
+                  >
+                    View The Project{" "}
+                    <span role="img" aria-label="right pointer">
+                      👉
+                    </span>
+                  </a>
+                </div>
+              </article>
+            ))}
         </section>
       </section>
     </main>
